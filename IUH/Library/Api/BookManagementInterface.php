e5aa65e4c4ddb753e9d938c5f06f56c1b6ba9407
NghiaLuuu <luutrungnghia1901@gmail.com>
2026-01-07 16:48:41 +0700

    Implement book management API methods: create, getList, and getById

--- IUH/Library/Api/BookManagementInterface.php
+++ IUH/Library/Api/BookManagementInterface.php
@@ -2,12 +2,31 @@
 namespace IUH\Library\Api;
 
 use IUH\Library\Api\Data\BookInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 interface BookManagementInterface
 {
     /**
+     * Create a new book
+     *
      * @param \IUH\Library\Api\Data\BookInterface $book
      * @return \IUH\Library\Api\Data\BookInterface
      */
-    public function create(BookInterface $book);
+    public function create(BookInterface $book): BookInterface;
+
+    /**
+     * Get list of all books
+     *
+     * @return \IUH\Library\Api\Data\BookInterface[]
+     */
+    public function getList(): array;
+
+    /**
+     * Get book by ID
+     *
+     * @param int $id
+     * @return \IUH\Library\Api\Data\BookInterface
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getById(int $id): BookInterface;
 }
--- IUH/Library/Model/BookManagement.php
+++ IUH/Library/Model/BookManagement.php
@@ -4,19 +4,49 @@ namespace IUH\Library\Model;
 use IUH\Library\Api\BookManagementInterface;
 use IUH\Library\Api\Data\BookInterface;
 use IUH\Library\Model\ResourceModel\Book as BookResource;
+use IUH\Library\Model\ResourceModel\Book\CollectionFactory;
+use IUH\Library\Model\BookFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class BookManagement implements BookManagementInterface
 {
     protected $bookResource;
+    protected $collectionFactory;
+    protected $bookFactory;
 
-    public function __construct(BookResource $bookResource)
-    {
+    public function __construct(
+        BookResource $bookResource,
+        CollectionFactory $collectionFactory,
+        BookFactory $bookFactory
+    ) {
         $this->bookResource = $bookResource;
+        $this->collectionFactory = $collectionFactory;
+        $this->bookFactory = $bookFactory;
     }
 
-    public function create(BookInterface $book)
+    public function create(BookInterface $book): BookInterface
     {
         $this->bookResource->save($book);
+        return $book;
+    }
+
+    public function getList(): array
+    {
+        $collection = $this->collectionFactory->create();
+        return $collection->getItems();
+    }
+
+    public function getById(int $id): BookInterface
+    {
+        $book = $this->bookFactory->create();
+        $this->bookResource->load($book, $id);
+
+        if (!$book->getId()) {
+            throw new NoSuchEntityException(
+                __('Book with id "%1" does not exist.', $id)
+            );
+        }
+
         return $book;
     }
 }
